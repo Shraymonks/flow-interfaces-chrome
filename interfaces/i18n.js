e92bb7d7535e04cec15e3ecbee67a797ee53ac87
Ross Allen <rossgit@gmail.com>
2017-12-18 01:56:22 -0800

    Match `getMessage` signature; extract types
    `DetectedLanguage` and `LanguageDetectionResult` are names that appear
    in documentation. Extract them as types to match the docs.

--- interfaces/i18n.js
+++ interfaces/i18n.js
@@ -1,14 +1,18 @@
 type chrome$LanguageCode = string;
 
+type chrome$DetectedLanguage = {
+  language: chrome$LanguageCode,
+  percentage: number,
+};
+
+type chrome$LanguageDetectionResult = {
+  isReliable: boolean,
+  languages: Array<chrome$DetectedLanguage>,
+};
+
 type chrome$i18n = {
-  getAcceptLanguages(callback: (Array<chrome$LanguageCode>) => void): void,
-  getMessage(messageName: string, ...substitutions?: Array<*>): ?string,
+  getAcceptLanguages(callback: (languages: Array<chrome$LanguageCode>) => void): void,
+  getMessage: (messageName: string, substitutions?: string | Array<string>) => string,
   getUILanguage(): string,
-  detectLanguage(text: string, callback: (result: {
-    isReliable: boolean,
-    languages: {
-      language: chrome$LanguageCode,
-      percentage: number,
-    },
-  }) => void): void,
+  detectLanguage(text: string, callback: (result: chrome$LanguageDetectionResult) => void): void,
 };

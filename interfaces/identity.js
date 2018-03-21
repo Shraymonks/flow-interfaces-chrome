cce39d8340b35eedac3ca3d66c5a58847d5668fe
Robert Niimi <robertn702@gmail.com>
2018-03-21 12:41:44 +0800

    Fix optional params from maybe types

--- interfaces/identity.js
+++ interfaces/identity.js
@@ -1,4 +1,4 @@
-type chrome$AccountInfo = string;
+type chrome$AccountInfo = { id: string };
 
 type chrome$identity = {
   getAccounts(callback: (accounts: Array<chrome$AccountInfo>) => void): void,
@@ -6,15 +6,15 @@ type chrome$identity = {
     interactive?: boolean,
     account?: chrome$AccountInfo,
     scopes?: Array<string>
-  }, callback?: (token: ?string) => void): void,
-  getProfileUserInfo(callback: (userInfo: { email: ?string, id: ?string }) => void): void,
+  }, callback?: (token?: string) => void): void,
+  getProfileUserInfo(callback: (userInfo: { email: string, id: string }) => void): void,
   removeCachedAuthToken(details: {
     token: string
   }, callback?: () => void): void,
   launchWebAuthFlow(details: {
     url: string,
     interactive?: boolean
-  }, callback: (responseUrl: ?string) => void): void,
+  }, callback: (responseUrl?: string) => void): void,
   getRedirectURL(path?: string): string,
   onSignInChanged: chrome$Event & {
     addListener(callback: (account: chrome$AccountInfo, signedIn: boolean) => void): void
